d6f57e097aae7e3c4f0a85723a4053fbe851d229
aidar.idrisov <matt.nolimiteron@gmail.com>
2021-05-07 16:06:05 +0300

    Добавление проверок полей

--- BottleWebProject1/myform.py
+++ BottleWebProject1/myform.py
@@ -1,7 +1,17 @@
 from bottle import post, request
+from re import *
 
 @post('/home', method='post')
 def my_form():
     mail = request.forms.get('ADRESS')
-    return "Thanks! The answer will be sent to the mail %s" % mail
-
+    quest = request.forms.get('QUEST')
+    pattern = compile('(^|\s)[-a-z0-9_.]+@([-a-z0-9]+\.)+[a-z]{2,6}(\s|$)')
+    is_valid = pattern.match(mail)
+    if not (mail == '' or quest == ''):
+        if is_valid:
+            return "Спасибо! Мы ответим вам по адресу %s" % mail
+        else:
+            return "Пожалуйста, введите правильный email. Иначе мы не сможем с вами связаться и не сможем сделать наш сайт чуточку лучше. :)"
+    else:
+        return "Пожалуйста, введите все поля. Похоже некоторые вы забыли ..."
+    
