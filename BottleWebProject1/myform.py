54e96b9ab2870c40b3865b54fa844becd5c60710
user1 <user1@A518-12>
2021-05-17 14:10:56 +0300

    корректирование записи в json файл

--- BottleWebProject1/myform.py
+++ BottleWebProject1/myform.py
@@ -2,24 +2,27 @@ from bottle import post, request
 from re import *
 import pdb
 import json
+questions = {}
 
 @post('/home', method='post')
 def my_form():
     mail = request.forms.get('ADRESS')
     quest = request.forms.get('QUEST')
-    questions = {}
     pattern = compile('(^|\s)[-a-z0-9_.]+@([-a-z0-9]+\.)+[a-z]{2,6}(\s|$)')
     is_valid = pattern.match(mail)
     if not (mail == '' or quest == ''):
         if is_valid:
-            questions[mail].append = quest
+            if (mail in questions):
+                questions[mail].append = quest
+            else:
+                questions[mail] = quest
+            with open('data.txt', 'w') as outfile:
+                json.dump(questions, outfile)
             return "Спасибо! Мы ответим вам по адресу %s" % mail
         else:
             return "Пожалуйста, введите правильный email. Иначе мы не сможем с вами связаться и не сможем сделать наш сайт чуточку лучше. :)"
     else:
         return "Пожалуйста, введите все поля. Похоже некоторые вы забыли ..."
-    with open('data.txt', 'w') as outfile:
-        json.dump(data, outfile)
 
     
 
